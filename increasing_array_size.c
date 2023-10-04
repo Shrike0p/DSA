a96dac00ff6287bafcec5b8db1a799ae42e77d40
Prakhar Singh <96379148+Shrike0p@users.noreply.github.com>
2023-10-04 23:35:41 +0530

    Update increasing_array_size.c

--- increasing_array_size.c
+++ increasing_array_size.c
@@ -30,3 +30,26 @@ int main(){
     return 0;
 }
 
+
+int main(){
+    int *p,*q;
+    
+    p=(int *)malloc(5*sizeof(int));
+    
+    p[0]=1;p[1]=1;p[2]=3;p[3]=3;p[4]=4;
+    
+    q=(int *)malloc(10*sizeof(int));
+    
+    for(int i=0; i<5; i++){
+        q[i]=p[i];
+    }
+    
+    free(p);
+    p=q;
+    q=NULL;
+    
+    for (int i=0;i<5;i++)
+        printf("%d ", p[i]);
+    return 0;
+}
+
