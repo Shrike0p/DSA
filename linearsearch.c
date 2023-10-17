3c7553688530b49863cda2747f52f5cfe9e468e7
Prakhar Singh <96379148+Shrike0p@users.noreply.github.com>
2023-10-17 16:07:06 +0530

    Update linearsearch.c

--- linearsearch.c
+++ linearsearch.c
@@ -16,17 +16,44 @@ void display(struct array arr){
     }
 }
 
-int LinearSearch(struct array arr,int key){
+/*int LinearSearch(struct array arr,int key){
     for(int i=0;i<arr.lenght;i++)
     if(key==arr.A[i]){
         return i;
         
     }
     return -1;
+}*/
+
+void swap(int *x, int *y){
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
 }
+//Transposition method of linear search
+int LinearSearch(struct array *arr,int key){
+    for(int i=0;i<arr->lenght;i++)
+    if(key==arr->A[i]){
+        swap(&arr->A[i],&arr->A[i-1]);
+        return i;
+    }
+    return -1;
+}
+
+/*Movie to front/ head method of linear search
+int LinearSearch(struct array *arr,int key){
+    for(int i=0;i<arr->lenght;i++)
+    if(key==arr->A[i]){
+        swap(&arr->A[i],&arr->A[0]);
+        return i;
+    }
+    return -1;
+}*/
+
 int main(){
     struct array arr={{2,3,4,5,6},5,10};
     
-    printf("%d\n",LinearSearch(arr,4));
+    printf("%d\n",LinearSearch(&arr,4));
     display(arr);
 }
